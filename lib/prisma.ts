df61173594e6b2c2706fa7ca99e33b44cc2bab47
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-03-13 19:55:44 +0700

    prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../node_modules/.prisma/client/index.js'
+import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient()
 async function main() {}
