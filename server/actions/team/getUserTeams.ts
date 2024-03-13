c3124783ec0935a08a1b2ae92d520fde16b3f457
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-03-13 22:18:58 +0700

    minor fix

--- server/actions/team/getUserTeams.ts
+++ server/actions/team/getUserTeams.ts
@@ -1,8 +1,11 @@
 'use server'
 
+import { cookies } from 'next/headers'
 import { getUserTeamsByUsername } from '@/server/db/team.data'
 
 export const getUserTeamsAction = async (username: string) => {
+    //const headers = cookies()
+    //console.log(headers)
     try {
         const res = await getUserTeamsByUsername(username)
         if (!res) throw new Error('user teams db query failed')
