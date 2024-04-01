d306c318aa2734076ac7ebdd4067ac967ab3f159
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-01 20:00:08 +0700

    removed get all server actions

--- server/actions/document/getDocument.ts
+++ server/actions/document/getDocument.ts
@@ -6,9 +6,9 @@ import { validateTeamMemberAction } from '../team/validateTeamMember'
 import { cookies } from 'next/headers'
 
 export const getDocumentAction = async (username: string, idName: string) => {
-    const sessionToken = cookies().getAll()
-    //if (!sessionToken) throw Error('no session token')
-    console.log('Session token ', sessionToken)
+    // const sessionToken = cookies().getAll()
+    // //if (!sessionToken) throw Error('no session token')
+    // console.log('Session token ', sessionToken)
     const user = await getUserBySessionTokenAction()
 
     try {
