dced0629656b1013f52c3c1b0dd308c3b60b44d8
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-01 19:36:27 +0700

    sesion token

--- server/actions/user/getUserBySessionToken.ts
+++ server/actions/user/getUserBySessionToken.ts
@@ -5,7 +5,7 @@ import { cookies } from 'next/headers'
 
 export const getUserBySessionTokenAction = async () => {
     const sessionToken = await cookies().get('next-auth.session-token')?.value
-    if (!sessionToken) throw Error('no session token')
-    const user = await getUserBySessionToken(sessionToken)
+    //if (!sessionToken) throw Error('no session token')
+    const user = await getUserBySessionToken(sessionToken!)
     return user
 }
