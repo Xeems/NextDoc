3e2b6416e4bf32f220062eda92ded569f2130c77
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-15 21:43:19 +0700

    markdown upload to db, tiptap editor delited

--- app/document/[owner]/[document]/edit/[...slug]/page.tsx
+++ app/document/[owner]/[document]/edit/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@/components/shadCn/ui/button'
 import {
     Tabs,
     TabsContent,
@@ -7,6 +8,8 @@ import {
     TabsTrigger,
 } from '@/components/shadCn/ui/tabs'
 import ReactMarkdown from '@/components/TextEditor/ReactMarkdown'
+import { useArticleQuery } from '@/hooks/querys/useArticleQuery'
+import { updateAricleContentAction } from '@/server/actions/article/updateAricleContent'
 import { notFound } from 'next/navigation'
 import React, { useState } from 'react'
 
@@ -22,11 +25,11 @@ const markdown = '#### Hello World'
 
 export default function DocumentPage({ params }: Props) {
     if (params.slug == undefined) notFound()
-    // const { data } = useArticleQuery({
-    //     documentName: params.document,
-    //     articleTitle: params.slug.slice(-1)[0],
-    // })
-    const [markdown, setMarkdown] = useState<string>('')
+    const { data: article } = useArticleQuery({
+        documentName: params.document,
+        articleTitle: params.slug.slice(-1)[0],
+    })
+    const [markdown, setMarkdown] = useState<string>(article?.content! || '')
     return (
         <div className="flex flex-col items-center">
             <div className="w-full flex flex-initial flex-row items-start justify-stretch gap-7 p-3 text-start min-w-[700px]">
@@ -36,11 +39,21 @@ export default function DocumentPage({ params }: Props) {
                     <TabsList className="w-fit">
                         <TabsTrigger value="editor">Editor</TabsTrigger>
                         <TabsTrigger value="preview">Preview</TabsTrigger>
+                        <Button
+                            onClick={async () => {
+                                const res = await updateAricleContentAction(
+                                    article!.id,
+                                    markdown,
+                                )
+                                console.log(res)
+                            }}>
+                            Save
+                        </Button>
                     </TabsList>
                     <TabsContent value="editor" className="w-full">
                         <textarea
                             autoFocus={true}
-                            className="min-h-40 outline-none w-full"
+                            className="min-h-40 outline-none w-full border rounded-md p-2"
                             value={markdown}
                             onChange={(e) => setMarkdown(e.target.value)}
                         />
