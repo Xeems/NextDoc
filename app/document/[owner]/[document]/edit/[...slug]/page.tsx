7ac9037cadf48e3d1451ffe89481062d0fd951a1
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-16 11:15:28 +0700

    tiptap packages removed

--- app/document/[owner]/[document]/edit/[...slug]/page.tsx
+++ app/document/[owner]/[document]/edit/[...slug]/page.tsx
@@ -9,9 +9,12 @@ import {
 } from '@/components/shadCn/ui/tabs'
 import ReactMarkdown from '@/components/TextEditor/ReactMarkdown'
 import { useArticleQuery } from '@/hooks/querys/useArticleQuery'
+import { getArticleByTitleAction } from '@/server/actions/article/getArticleByTitle'
 import { updateAricleContentAction } from '@/server/actions/article/updateAricleContent'
+import { useMutation } from '@tanstack/react-query'
 import { notFound } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 
 type Props = {
     params: {
@@ -21,15 +24,39 @@ type Props = {
     }
 }
 
-const markdown = '#### Hello World'
-
 export default function DocumentPage({ params }: Props) {
     if (params.slug == undefined) notFound()
-    const { data: article } = useArticleQuery({
-        documentName: params.document,
-        articleTitle: params.slug.slice(-1)[0],
-    })
-    const [markdown, setMarkdown] = useState<string>(article?.content! || '')
+    // const { data: article } = useArticleQuery({
+    //     documentName: params.document,
+    //     articleTitle: params.slug.slice(-1)[0],
+    // })
+    const [markdown, setMarkdown] = useState<string>('')
+
+    useEffect(() => {
+        ;async () => {
+            const res = await getArticleByTitleAction({
+                documentName: params.document,
+                articleTitle: params.slug.slice(-1)[0],
+            })
+            console.log(res)
+            setMarkdown(res.data?.content!)
+        }
+    }, [])
+
+    // const mutation = useMutation({
+    //     mutationKey: ['article', params.document, params.slug.slice(0)[-1]],
+    //     mutationFn: () => updateAricleContentAction(article!.id, markdown),
+    // })
+
+    // const handleSave = async () => {
+    //     try {
+    //         await mutation.mutateAsync() // Trigger the mutation
+    //         toast.success('Article content updated successfully!')
+    //     } catch (error) {
+    //         toast.error('Error updating article content')
+    //     }
+    // }
+
     return (
         <div className="flex flex-col items-center">
             <div className="w-full flex flex-initial flex-row items-start justify-stretch gap-7 p-3 text-start min-w-[700px]">
@@ -39,24 +66,17 @@ export default function DocumentPage({ params }: Props) {
                     <TabsList className="w-fit">
                         <TabsTrigger value="editor">Editor</TabsTrigger>
                         <TabsTrigger value="preview">Preview</TabsTrigger>
-                        <Button
-                            onClick={async () => {
-                                const res = await updateAricleContentAction(
-                                    article!.id,
-                                    markdown,
-                                )
-                                console.log(res)
-                            }}>
-                            Save
-                        </Button>
+                        <Button type="submit">Save</Button>
                     </TabsList>
                     <TabsContent value="editor" className="w-full">
-                        <textarea
-                            autoFocus={true}
-                            className="min-h-40 outline-none w-full border rounded-md p-2"
-                            value={markdown}
-                            onChange={(e) => setMarkdown(e.target.value)}
-                        />
+                        <form>
+                            <textarea
+                                autoFocus={true}
+                                className="min-h-40 outline-none w-full border rounded-md p-2"
+                                value={markdown}
+                                onChange={(e) => setMarkdown(e.target.value)}
+                            />
+                        </form>
                     </TabsContent>
                     <TabsContent value="preview" className="">
                         <ReactMarkdown markdown={markdown} />
