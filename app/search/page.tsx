778a34db938b85c91a11c08450dfe4cb2cb0dca9
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-18 22:40:44 +0700

    search page init

---
+++ app/search/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SearchFrom from './SearchFrom'
+
+function SearchPage() {
+    return (
+        <div className="w-full ">
+            <div className="flex w-1/3 mx-auto">
+                <SearchFrom />
+            </div>
+        </div>
+    )
+}
+
+export default SearchPage
