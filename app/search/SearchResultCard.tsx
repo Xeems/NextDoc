57a964dcfe911d025f6c456b4fe2bd501ccd8b06
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-25 22:23:35 +0700

    seqrch page started

---
+++ app/search/SearchResultCard.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@/components/shadCn/ui/card'
+import React from 'react'
+
+type Props = {
+    searchResult: UserType | DocType | TeamType
+}
+function SearchResultCard() {
+    return <Card className="w-full"></Card>
+}
+
+export default SearchResultCard
---
+++ app/search/SerachResult.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+type Props = {
+    data?: unknown[]
+}
+function SerachResult({ data }: Props) {
+    if (data != undefined) return <></>
+}
+
+export default SerachResult
