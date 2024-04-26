2c7e7326436d0377a3fb8ffdf36bec7bfde6ef10
roman krasilnikov <krasilnikovroman5@gmail.com>
2024-04-26 22:20:37 +0700

    search page

---
+++ server/actions/paginationSearch.tsx
@@ -0,0 +1,35 @@
+'use server'
+
+import { searchSchema, SearchType } from '@/@types/validators/search'
+
+import { paginationDocumentsSearch } from '../db/document.data'
+
+type Props = {
+    data: SearchType
+    page: number
+}
+export const paginationSearchAction = async ({ data, page }: Props) => {
+    const validationResult = await searchSchema.safeParseAsync(data)
+
+    if (!validationResult.success)
+        throw new Error(
+            `Server side validation failed ${validationResult.error.issues[0].message}`,
+        )
+
+    try {
+        // let res
+        // switch (data.searchTarget) {
+        //     case 'documents': {
+        //         res = await paginationDocumentsSearch(data.searchQuery, page)
+        //     }
+        //     default: {
+        //         res = await paginationDocumentsSearch(data.searchQuery, page)
+        //     }
+        // }
+        const res = await paginationDocumentsSearch(data.searchQuery, page)
+        return { data: res }
+    } catch (error) {
+        console.log(error)
+        return { error: 'Something went wrong' }
+    }
+}
